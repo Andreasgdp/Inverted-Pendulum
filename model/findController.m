6befd45b31189ed21e2122591c5756dc1c4367d7
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-05-03 10:06:00 +0200

    Can tune now so thats something

--- model/findController.m
+++ model/findController.m
@@ -24,22 +24,30 @@ ks = kp*(1 + ti/(s) + td*s);
 % figure(2)
 % step(ks*G_p/(1 + ks*G_p))
 
-%% Cart transfer function controller
+%% Cart transfer function controller (CAN'T SEEM TO GET THIS ONE TO STABILIZE)
 
 G_c = 0.2401*s^2 + 0.0012*s - 2.887/(0.2353*s^4 + 1.202*s^3 - 3.863*s^2 - 14.44*s);
 
 kp = 1.72;
 
-ti = 0.8;
+ti = -0.5;
 
 td = 0.1;
 
 ksFind = (1 + ti/(s) + td*s);
 ks = kp*(1 + ti/(s) + td*s);
 
-figure(1)
-rlocus(ksFind*G_c)
-sisotool(ksFind*G_c)
+z = [-3 -4];
+p = 0;
+k = 1;
+C = zpk(z,p,k)
 
-figure(2)
-step(ks*G_c/(1 + ks*G_c))
+pole(C*G_c)
+zero(C*G_c)
+
+% figure(1)
+rlocus(G_c)
+% sisotool(ksFind*G_c)
+% 
+% figure(2)
+% step(ks*G_c/(1 + ks*G_c))
